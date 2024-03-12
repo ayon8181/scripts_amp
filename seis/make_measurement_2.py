1af8a8d2777a2f119671795a6879bf17da832dac
ayon8181 <ayon.8181@gmail.com>
2024-03-11 21:35:43 -0500

    Added filters for Mesurement

--- seis/make_measurement_2.py
+++ seis/make_measurement_2.py
@@ -7,8 +7,10 @@ from scipy.signal import hilbert
 from obspy.taup import TauPyModel
 from obspy.geodetics import locations2degrees
 from obspy.signal.cross_correlation import correlate_template
+from obspy.imaging import beachball
 from scipy import fft
 from pyasdf import ASDFDataSet
+from geographiclib.geodesic import Geodesic
 import time
 import sys
 import math
@@ -18,8 +20,68 @@ event       = str(sys.argv[1])
 obsd_tag    = str(sys.argv[2])
 obsd_fname  = str(sys.argv[3])
 
+def rad_pat_sh(rake,dip,az,st,tk_of):
+    y = (np.cos(rake)*np.cos(dip)*np.cos(tk_of)*np.sin(az-st) +
+       np.cos(rake)*np.sin(dip)*np.sin(tk_of)*np.cos(2*(az-st)) +
+       np.sin(rake)*np.cos(2*dip)*np.cos(tk_of)*np.cos(az-st) -
+       0.5*np.sin(rake)*np.sin(2*dip)*np.sin(tk_of)*np.sin(2*(az-st)))
+    return y
 
-def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):                 #event_loc=[evla,evlo,evdp]
+def nodal_plane_filter(event):
+    mt    = event.focal_mechanisms[0].moment_tensor.tensor
+    #nod_p = beachball.mt2plane(beachball.MomentTensor(0,1,-1,0,0,0,26))
+    nod_p = beachball.mt2plane(beachball.MomentTensor(mt.m_rr,mt.m_tt,mt.m_pp,mt.m_rt,mt.m_rp,mt.m_tp,0))
+    strike= nod_p.strike
+    dip   = nod_p.dip
+    rake  = nod_p.rake
+    l=0
+    np_0=[]
+    az=[]
+    np_1=[]
+    for i in range(1,360):
+        rp=np.abs(rad_pat_sh(np.deg2rad(rake),np.deg2rad(dip),np.deg2rad(i),np.deg2rad(strike),np.deg2rad(20.0)))
+        if rp<=0.001:
+           np_0.append(i)       
+    return np_0
+
+def plane(nodal):
+    if nodal<0:
+       y=360+nodal
+    elif nodal>=360:
+         y=0+(nodal-360)
+    else:
+         y=nodal
+    
+    return y
+
+def SNR(noise,data):
+    n_rate    = noise[1]-noise[0]
+    d_rate    = data[1]-data[0]
+    n_power   = integrate.trapezoid(noise**2,dx=n_rate)/(len(noise)*n_rate)
+    d_power   = integrate.trapezoid(data**2,dx=d_rate)/(len(d_power)*d_rate)
+    rat_power = d_power/n_power
+
+    m_noise   = max(np.abs(noise))
+    m_data    = max(np.abs(data))
+
+    rat_max   = m_data/m_noise
+
+    return rat_power, rat_max
+
+def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag,nod_p,noise_acc):                 #event_loc=[evla,evlo,evdp]
+    """
+    Error Code:
+    1: Rejected as the station is on a nodal plane
+    2: Fails Global Signal to Noise Ratio Filters
+
+    Error for each phase:
+    0: Succesfully measured windows
+    1: No arrival for that phase
+    2: Signal to Noise Ratio Fails for that phase
+    3: Correlation Value Error
+    4: Time Shift greater than 15 seconds
+    5: Signal to Noise Ratio Fails for time_shift windows
+    """
     model=TauPyModel(model="prem")
     #create output arrays
     max_cc       = np.full(len(phase_list),np.nan)
@@ -32,18 +94,31 @@ def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):           
     A1           = np.full(len(phase_list),np.nan)
     A2           = np.full(len(phase_list),np.nan)
     arrival_times= np.full(len(phase_list),np.nan)
+    success      = np.full(len(phase_list),np.nan)
 
     ## Read the station and event information
     sta    = stationXML
     stla   = sta[0][0].latitude
     stlo   = sta[0][0].longitude
     stel   = sta[0][0].elevation
+    name   = sta[0].code+sta[0][0].code
     evla   = event_loc[0]
     evlo   = event_loc[1]
     evdp   = event_loc[2]
+
     
 
-    gcarc= locations2degrees(stla, stlo, evla, evlo)
+    gcarc  = locations2degrees(stla, stlo, evla, evlo)
+    geod   = Geodesic.WGS84.Inverse(evla,evlo,stla,stlo)
+    az     = geod["azi1"]
+
+    ##Nodal Plane Filtering
+    for p in nod_p:
+        if plane(az) >= plane(p-7.5) and plane(az) <= plane(p+7.5):
+            
+            return [1,name]
+
+
     if gcarc >= 10:
     
         ## get the waveforms
@@ -56,6 +131,38 @@ def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):           
             synt_anltc    = hilbert(synt_data.data)
             obsd_envp     = np.abs(obsd_anltc)
             synt_envp     = np.abs(synt_anltc)
+
+            
+            
+            arrival = model.get_travel_times(source_depth_in_km=evdp,distance_in_degree=gcarc,phase_list=["P"])
+            if len(arrival) != 0:
+               arrival_time = arrival[0].time
+
+               arr_index    = int(arrival_time*sampling_rate)
+               n_start_index= int(arr_index-125*sampling_rate)
+               n_end_index  = int(arr_index-25*sampling_rate)
+    
+            elif len(arrival) == 0:
+                 arrival = model.get_travel_times(source_depth_in_km=evdp,distance_in_degree=gcarc,phase_list=["Pdiff"])
+                 if len(arrival) != 0:
+                    arrival_time = arrival[0].time
+
+                    arr_index    = int(arrival_time*sampling_rate)
+                    n_start_index= int(arr_index-125*sampling_rate)
+                    n_end_index  = int(arr_index-25*sampling_rate)
+            
+            o_noise  = obsd_data.data[n_start_index:n_end_index]
+            s_noisen = synt_data.data[n_start_index:n_end_index]
+
+            ## Check Global Data Quality
+
+            g_i, g_m = SNR(o_noise,obsd_data)
+
+            if g_i <= noise_acc[0] and g_m <= noise_acc[1]:
+               
+               return [2,name]
+
+
 
             for i,phase in enumerate(phase_list):
                 arrivals = model.get_travel_times(source_depth_in_km=evdp,distance_in_degree=gcarc,phase_list=[phase])
@@ -70,16 +177,38 @@ def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):           
                     synt_wdow    = synt_data.data[start_index:end_index]
                     en_synt_wdow = synt_envp[start_index:end_index]
                     obsd_cut     = obsd_data.data[int(start_index-50*sampling_rate):int(end_index+50*sampling_rate)]
+
+
+                    ## Filter Data on SNR
+                    w_i, w_m = SNR(o_noise,obsd_cut)
+                    if w_i <= noise_acc[2] and w_m <= noise_acc[3]:
+                        success[i] = 2
+                        continue 
+
+                    ## Calculate cross-correlation co-efficients
                     try:
                         cc           = correlate_template(obsd_cut,synt_wdow,mode="valid",normalize="full",method="auto")
                     except ValueError:
-                        return
+                        success[i] = 3
+                        continue
                     max_ind      = np.argmax(cc)
                     max_cc_val   = cc[max_ind]
                     shift        = int(max_ind - int(len(cc)/2)-1)
                     time_shift   = shift/sampling_rate
+
+                    ## Filter out based on cross-correlation value
+                    if np.abs(time_shift) >= 15:
+                       success[i] = 4
+                       continue
+
                     obsd_wdow    = obsd_data[start_index+shift:end_index+shift]
                     en_obsd_wdow = obsd_envp[start_index+shift:end_index+shift]
+
+                    w_i, w_m = SNR(o_noise,obsd_wdow)
+                    if w_i <= noise_acc[2] and w_m <= noise_acc[3]:
+                        success[i] = 5
+                        continue 
+                    
 
                     ## Make Measurements on the two windows
                     sqrd_obsd    = integrate.trapz(obsd_wdow*obsd_wdow, dx=dt)
@@ -105,6 +234,9 @@ def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):           
 
                     A1[i]           = obsd_synt_val/sqrd_synt
                     A2[i]           = sqrd_obsd/obsd_synt_val
+                    success[i]      = 0
+                else:
+                     success[i] = 1
             
             SS_S            = 0.5*(min(A1[1],A2[1])/max(A1[0],A2[0]) + max(A1[1],A2[1])/min(A1[0],A2[0]))
             SSS_SS          = 0.5*(min(A1[2],A2[2])/max(A1[1],A2[1]) + max(A1[2],A2[2])/min(A1[1],A2[1]))
@@ -125,9 +257,9 @@ def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):           
                 output.append(env_rat_3d1d[j])
                 output.append(0.5*(A1[j]+A2[j]))
             
-            return output
+            return [0,name,output,success]
         except IndexError:
-               return None
+               return [3,name]
 
 
 
@@ -135,6 +267,8 @@ def make_measure(obsd,synt,stationXML,phase_list,event_loc,obsd_tag):           
 
 ds        = ASDFDataSet("/scratch1/09038/ayon8181/pypaw_workflow_test/seis/proc/"+event+".T017-040s.proc_"+obsd_fname+".h5",mode="r")
 ds2       = ASDFDataSet("/scratch1/09038/ayon8181/pypaw_workflow_test/seis/proc/"+event+".T017-040s.proc_real_data.h5",mode="r")
+
+noise_acc = [3.5,6.0,3,3]
 #print("/scratch1/09038/ayon8181/pypaw_workflow_test/seis/proc/"+event+".T017-050s.proc_"+obsd_fname+".h5")
 
 evla      = ds.events[0].origins[0].latitude
@@ -142,6 +276,8 @@ evlo      = ds.events[0].origins[0].longitude
 evdp      = ds.events[0].origins[0].depth/1000.0
 evloc     = [evla,evlo,evdp]
 phase_list= ["S","SS","SSS","ScS","Sdiff"] 
+
+nod_p     = nodal_plane_filter(ds.events[0])
 
 
 def process(this_station_group, other_station_group):
@@ -159,7 +295,7 @@ def process(this_station_group, other_station_group):
 
     all_results= []
 
-    results    = make_measure(observed,synthetic,stationxml,phase_list,evloc,obsd_tag)
+    results    = make_measure(observed,synthetic,stationxml,phase_list,evloc,obsd_tag,nod_p,noise_acc)
 
 
     all_results.append(results)
@@ -175,13 +311,25 @@ b          = time.time
 
 if ds.mpi.rank == 0:
     #print(all_output)
-    with open("/scratch1/09038/ayon8181/pypaw_workflow_test/outputs/"+obsd_fname+"_w_real.txt4",'a') as txt:
+    with open("/scratch1/09038/ayon8181/pypaw_workflow_test/outputs/"+obsd_fname+"_real.txt",'a') as txt, \
+         open("/scratch1/09038/ayon8181/pypaw_workflow_test/errors/error_"+obsd_fname+"_phase.txt",'a') as txt2, \
+         open("/scratch1/09038/ayon8181/pypaw_workflow_test/errors/error_"+obsd_fname+"_all.txt",'a') as txt3:
         for k in all_output.keys():
-            if all_output[k][0] is not None:
+            if all_output[k][0][0] == 0:
                 txt.write(event+" "+k+" ")
-                for h in all_output[k][0]:
+                for h in all_output[k][0][2]:
                     txt.write(str(h)+" ")
                 txt.write("\n")
+
+                for j,error in enumerate(all_output[k][0][3]):
+                    if error is not np.nan and error != 0:
+                        txt2.write(event+" "+all_output[k][0][1]+" "+phase_list[j]+" "+error+"\n")
+                
+
+            elif all_output[k][0][0] != 0:
+                
+                txt3.write(event+" "+all_output[k][0][1]+" "+all_output[k][0][0]+"/n")
+            
 del ds
 del ds2    
 
